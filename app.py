dc5ee9280d775014b3b887471071d672abb64831
Silvio <silviolima@gmail.com>
2021-01-18 18:05:02 -0300

    Update CSV ->18/01/2021 18:05

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ def main():
     elif choice == 'About':
         #st.sidebar.image(about,caption="", width=300, height= 200)
         st.subheader("Built with Streamlit")
+        
         st.write("Dados coletados via scrap usando: Selenium e BeautifulSoup.")
         st.markdown("A coleta dos dados é feita às 9h, 12h, 15h e 18h")
         st.write("Nestes horários, via crontab scripts realizam o scrap e atualização do app.")
@@ -144,6 +145,7 @@ def main():
         st.write("O scrap para o cargo de Engenheiro de Machine Learning trouxe poucas linhas.")
         st.write("Para os demais cargos, foram encontradas mais de 100 vagas, distribuídas em diversas páginas.")
         st.write("Esse app traz as 10 primeiras páginas apenas.")
+        st.markdown("Versão 02: incluido link da vaga")
         st.subheader("by Silvio Lima")
         
         if st.button("Linkedin"):
