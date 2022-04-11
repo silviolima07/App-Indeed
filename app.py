ef62239e15f23ea5a9d0d423c347a2434bca1a04
Silvio <silviolima@gmail.com>
2022-04-11 01:59:42 -0300

    update app

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def get_table_download_link(df,file):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}" download= "{file}" >Download csv</a>'
+    return href
     
 
 def make_clickable(link):
